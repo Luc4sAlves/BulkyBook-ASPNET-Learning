47584f1401c6f7da21fb8ab672bd8ea44877994d
Jose Lucas <jlucas.alvesg@gmail.com>
2022-03-10 15:14:09 -0300

    Adicionando controlador category e recuperando elementos da Db.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            //Pegando os elementos do database, passando pra uma lista e alocando em uma variavel
+            var objCategoryList = _db.Categories.ToList(); 
+            return View();
+        }
+    }
+}
