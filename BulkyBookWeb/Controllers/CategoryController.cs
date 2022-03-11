8c3b2b15a3862bb62e78f4bf03ae879c108b37ae
Jose Lucas <jlucas.alvesg@gmail.com>
2022-03-11 15:46:40 -0300

    adicionado método get para Edit Category

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -48,5 +48,44 @@ namespace BulkyBookWeb.Controllers
             }
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id.Value == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id.Value);
+            //Podia usar firstordefault ou singleordefault ao inves de find
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                //Colocando como name ali no primeiro argumento, ele mostra o erro no campo name no form
+                //Colocando CustomError, ele mostra lá em cima, como foi definido no create html
+                ModelState.AddModelError("name", "The name and display order can't be the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
+
+
+
 }
